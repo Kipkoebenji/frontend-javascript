7943e059971b06a1868bf1fc80c5eb74ba153a39
Benjamin <kipkoech9275@gmail.com>
2026-06-25 11:33:38 +0300

    feat: add mock api calls

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import RegisterForm from './RegisterForm.jsx'
 import MockApi from './MockApi.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RegisterForm />
+  <StrictMode>    
     <MockApi />
   </StrictMode>,
 )
