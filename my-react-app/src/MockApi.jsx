5ebcb36020d82fa3776d79bcd68bc86078a2a138
Benjamin <kipkoech9275@gmail.com>
2026-06-25 11:32:31 +0300

    feat: add mock api calls

--- my-react-app/src/MockApi.jsx
+++ my-react-app/src/MockApi.jsx
@@ -1,10 +1,41 @@
-
+import { useEffect, useState } from "react";
 
 export default function MockApi() {
+
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            const result = await response.json();
+            setData(result);
+        } catch (error) {
+            setError(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
   
     return (
     <div>
-      
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      {data && (
+        <ul>
+          {data.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import RegisterForm from './RegisterForm.jsx'
+import MockApi from './MockApi.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RegisterForm />
+    <MockApi />
   </StrictMode>,
 )
